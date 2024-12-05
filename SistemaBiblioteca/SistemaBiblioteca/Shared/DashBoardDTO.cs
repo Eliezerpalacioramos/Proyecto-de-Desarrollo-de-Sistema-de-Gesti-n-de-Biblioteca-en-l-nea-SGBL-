c5c533a314c82ceb081f559dd261def4b172e4b2
Kiara Nicole Custodio Cruz <20230276@itla.edu.do>
2024-12-04 22:16:20 -0600

    ok

---
+++ SistemaBiblioteca/SistemaBiblioteca/Shared/DashBoardDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBiblioteca.Shared
+{
+    public class DashBoardDTO
+    {
+        public int TotalLibros { get; set; }
+        public int TotalLectores { get; set; }
+        public int PrestamosRegistrados { get; set; }
+        public int PrestamosPendientes { get; set; }
+
+    }
+}
